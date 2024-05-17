271b784bcdaed741119a5a2a19af148d6f5313b3
anushiyaponnusamy <anushiyaponnusamy@gmail.com>
2024-05-17 12:23:45 +0530

    addde changes

--- utils/uploadImage.js
+++ utils/uploadImage.js
@@ -1,4 +1,3 @@
-
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const path = require('path');
@@ -9,31 +8,41 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
 // Multer configuration for file upload
 const storage = multer.memoryStorage(); // Store the file in memory
 const upload = multer({ storage: storage });
 
 const uploadMiddleware = async (req, res, next) => {
   try {
+    // Handle multer file upload
     await new Promise((resolve, reject) => {
       upload.single('image')(req, res, (err) => {
         if (err instanceof multer.MulterError) {
-          return res.status(400).json({ error: 'Error uploading image' });
+          reject(new Error('Error uploading image'));
         } else if (err) {
-          return res.status(500).json({ error: 'Server error' });
+          reject(new Error('Server error'));
+        } else {
+          resolve();
         }
-
-        resolve();
       });
     });
+
     if (!req.file) {
       return res.status(400).json({ error: 'Missing file in the request' });
     }
+
+    // Convert file buffer to base64 string
     const base64 = req.file.buffer.toString('base64');
-    const result = await cloudinary.uploader.upload(`data:${req.file.mimetype};base64,${base64}`, {
+    const dataUri = `data:${req.file.mimetype};base64,${base64}`;
+
+    // Upload file to Cloudinary
+    const result = await cloudinary.uploader.upload(dataUri, {
       resource_type: 'auto', // 'auto' detects the file type
       public_id: path.parse(req.file.originalname).name // Set your desired filename here
     });
+
+    // Attach the Cloudinary URL to the request object
     req.imageUrl = result.secure_url;
     next();
   } catch (error) {
@@ -42,8 +51,4 @@ const uploadMiddleware = async (req, res, next) => {
   }
 };
 
-
-
 module.exports = uploadMiddleware;
-
-
